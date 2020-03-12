c676902467bf25cda45fa3f484d62ccc279ee395
louisedgar <louisedgar@ymail.com>
2020-03-12 19:48:52 +0700

    first init

---
+++ src/data.js
@@ -0,0 +1,134 @@
+[
+  {
+    id: 1,
+    title: "Brown Ceramic Bowl",
+    creator: "New Norm",
+    img: "img/product-1.png",
+    price: 10,
+    info:
+      "Handmade ceramic bowl, made from black porcelain clay and hand-brushed with gloss glaze. Our ‘classic’ bowls have a straighter edge than our ‘pebble’ bowls, which are more flared and irregular shaped. This makes them suitable for a variety of dishes as shown above. Both style bowls will make excellent additions to your dinner setting. Each piece is created using a technique called slipcasting, where liquid clay is poured into a plaster mould.The outside of the bowl is left unglazed, creating a beautiful subtle contrast and tactility.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Pearl Ceramic Mug",
+    creator: "Nord Wood",
+    img: "img/product-2.png",
+    price: 10,
+    info:
+      "Gorgeous handmade ceramic mug, made from white porcelain clay and hand-glazed with a white gloss glaze. This elegant shape roughly holds a generous 380ml (over 13.5oz), the perfect coffee cup for large cappuccino’s or use for your daily cups of tea. Each piece is created using a technique called slipcasting, where liquid clay is poured into a plaster mould, left to set for the desired thickness and the excess slip poured out.The inside and rim of the mug are glazed with our non- toxic, food - safe white glaze.The outside base of the cup is left unglazed, creating a beautiful subtle contrast.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Brown Ceramic Cup",
+    creator: "New Norm",
+    img: "img/product-3.png",
+    price: 10,
+    info:
+      "Gorgeous handmade ceramic cup, made from black porcelain clay and hand-dipped in our earthy matte riverstone glaze. This little cutie roughly holds about 5oz, as with any handmade item variation will exist in size. Each piece is created using a technique called slipcasting, where liquid clay is poured into a plaster mould, left to set for the desired thickness and the excess slip poured out.The inside of the mug is glazed with our non- toxic, food - safe clear glaze before being dipped in our riverstone glaze.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Minimal Chess",
+    creator: "Lanier Graham",
+    img: "img/product-4.png",
+    price: 15,
+    info:
+      "Have a look at this modern chess set by Lanier Graham, which presents a minimal yet functional design to keep you busy and entertained on the go. An official part of MoMA’s collection, the portable chess set includes pieces designed to reflect their relative importance and the range of mobility on the board. Featuring the pieces made in cherry and maple wood, with a natural oil finish, to differentiate each side.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "French Press",
+    creator: "Yield Design Co",
+    img: "img/product-5.png",
+    price: 25,
+    info:
+      "A large ceramic water jug, with a clear glazed interior an unglazed polished finish. Individually slipcast from colored porcelain and polished to a smooth pebble- like finish.The porcelain is high quality and fired at extremely high temperatures, making it the strongest and most durable material for dishes. As the jug doesn't have any glaze, please take care with wooden surfaces as condensation can occur on the outside of the vessel. Each piece is individually made and will have slight variations, indicative of the material and hand made processes.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Brown Steel Floor Lamp",
+    creator: "Jonny Caspari",
+    img: "img/product-6.png",
+    price: 75,
+    info:
+      "Light up your bedroom, office, foyer, or living room with this elegant one-light torchiere floor lamp. Crafted from steel, it features a streamlined design and two-tone metallic finish for a look that works equally well in industrial lofts and modern farmhouses. Its single light is highlighted by a small cylindrical shade, directing the gleam at your desired angle, perfect for reading books and studies alike.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Pendant Lamp",
+    creator: "Pierre Chatel",
+    img: "img/product-7.png",
+    price: 40,
+    info:
+      "Handy for giving you on-trend lighting in spaces with low ceilings, flush and semi-flush mount lights are a great option for brightening up space where square footage is at a premium. Take this one, for example: perfect for an industrial-inspired touch, this piece features a wide cone shade crafted from metal with a bronze finish and understated, classic silhouette. Perfect for washing a specific area in light, this piece features a downlight design.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Apsis Lamp",
+    creator: "Zack Stratfold",
+    img: "img/product-8.png",
+    price: 40,
+    info:
+      "Apsis is based on the circle’s most fundamental quality – rotational symmetry. The laminated beech wood body revolves freely around a limestone trey base, without the need for the complicated mechanisms commonly associated with adjustable lamps. In placing the bottom ring over the circular stone base, a basic bearing is created, allowing the lamp to endlessly rotate. The cable and LED components are hidden within the body and head, creating a clean, uninterrupted aesthetic that emphasises the lamp’s simple geometry.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Orange Chair",
+    creator: "Rabbie Madaci",
+    img: "img/product-9.png",
+    price: 50,
+    info:
+      "A clean, fresh modern take on the traditional Windsor chair. This beautifully styled dining chair is wire-brushed to bring out the cathedral grain in the oak, and stained in a choice of sleek orange, walnut brown, or blonde oak finish options. Welcoming classic and postmodern contrast to industrial décor.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "Minimal Stool",
+    creator: "Ruslan Bardash",
+    img: "img/product-10.png",
+    price: 35,
+    info:
+      "The Bardash's reclaimed counter-stool embodies true rustic appeal with its characteristic frame created from reclaimed pine wood. Every panel of the stool tells its own story, emphasized by kaleidoscopic color variations and natural markings on the surface. The tapered legs and wooded bars provide for distinctive style and functionality. The pine wood used is FSC certified and meets universal sustainability standards.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "The Leaves",
+    creator: "Emma Pollard",
+    img: "img/product-11.png",
+    price: 85,
+    info:
+      "Enjoy the world’s finest art that will breathe life to any room or setting. Choose from our beautiful selection of high-quality frames to go with the right art that fits you. All prints arrive ready to hang.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
