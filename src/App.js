db8efd3bd59a482698b774a58c2f8d87ea41d74a
louisedgar <louisedgar@ymail.com>
2020-03-13 18:55:44 +0700

    add styled-components dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Logo from "./logo.svg";
 
 function App() {
   return (
     <div className="container">
       <header>
-        <h1>SIMPLISM</h1>
+        <img src={Logo} alt="" />
       </header>
     </div>
   );
